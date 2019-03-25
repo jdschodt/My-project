b9c822854795e0ae00493b2a0e0db7000958a4c5
reinoutlavens <reinoutlavens@gmail.com>
2019-03-25 14:35:21 +0100

    hallo

--- sensortest/src/pack/UltrsonicSensor.java
+++
@@ -1,9 +0,0 @@
-package pack;
-
-import lejos.hardware.ev3.LocalEV3;
-import lejos.hardware.port.Port;
-import lejos.hardware.sensor.EV3UltrasonicSensor;
-import lejos.robotics.SampleProvider;
-
-public class UltrsonicSensor {
-}
